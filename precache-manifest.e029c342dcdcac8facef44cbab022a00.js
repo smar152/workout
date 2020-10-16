5848bcb97c9770ad2dc5b88303803f3eed64b621
smar152 <smar152@users.noreply.github.com>
2020-10-16 14:06:19 +0300

    Updates

---
+++ precache-manifest.e029c342dcdcac8facef44cbab022a00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a7d9b47864ab9a672f2daf63592741c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "313327a3886e6fd57563",
+    "url": "/static/css/main.a14d5126.chunk.css"
+  },
+  {
+    "revision": "b641d886fdf399543881",
+    "url": "/static/js/2.dcb694d7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.dcb694d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "313327a3886e6fd57563",
+    "url": "/static/js/main.9db835be.chunk.js"
+  },
+  {
+    "revision": "e6b76d7dcdc2e453bc25",
+    "url": "/static/js/runtime-main.511a283d.js"
+  }
+]);
